3e21d9f3819e1922813a3ad4e5f06d775e1ec94d
Anna Tatum <purelle626@gmail.com>
2021-02-03 22:51:49 -0600

    Models of Part 2: Persisting Employers and Skills complete. imported @Size class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message ="Skill description is required")
+    //@Size(min = 3, max= 255, message= "Skill description must be between 3 and 255 characters")
+    private String skillDescription;
+
+    public Skill(String skillDescription){
+        this.skillDescription = skillDescription;
+    }
+
+    public Skill(){};
+
+
+
+    public String getSkill() {
+        return skillDescription;
+    }
+
+    public void setSkill(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 
 }
